b2e8c96b2e602e1971d5b948325ac844f077040b
victorious-messaging-4309 <wildayk@gmail.com>
2019-03-31 20:00:49 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,7 +3,8 @@ require 'pry'
 html = File.read('fixtures/kickstarter.html')
 
 kickstarter = Nokogiri::HTML(html)
+    binding.pry
 
 def create_project_hash
-  # write your code here
+# write your code here
 end
